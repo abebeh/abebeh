d821425b1ed32b0207e9fb3c2cb89104dd3c2b25
abebeh <abusadou15@gmail.com>
2023-06-25 15:43:34 +0900

    カリキュラム8-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; 
 use App\Models\Post;
 
 class PostController extends Controller
@@ -12,9 +13,9 @@ class PostController extends Controller
         return view('posts.index')->with(['posts'=>$post->getPaginateBylimit(1)]);
     }
     
-    public function show(Post $post)
+    public function show()
     {
-        return view('posts.show')->with(['post' => $post]);
+        return view('posts.create');
     }
     
     public function create()
@@ -22,7 +23,7 @@ class PostController extends Controller
         return view('posts.create');
     }
     
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
